fc53b261201ad4faa09e2680fdb02709c4a33bca
Jono Morris <jono.morris@xtra.co.nz>
2023-03-10 14:40:04 +1300

    Update app configuration

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO tasks VALUES (default, 'task to do', 'future pending task', '2023-05-02', 'PENDING', '2023-03-05');
-INSERT INTO tasks VALUES (default, 'another task to do', 'future pending task', '2023-04-02', 'PENDING', '2023-03-05');
-INSERT INTO tasks VALUES (default, 'yet another task to do', 'future pending task', '2023-02-10', 'PENDING', '2023-03-05');
+--INSERT INTO tasks VALUES (default, 'another task to do', 'future pending task', '2023-04-02', 'PENDING', '2023-03-05');
+--INSERT INTO tasks VALUES (default, 'yet another task to do', 'future pending task', '2023-02-10', 'PENDING', '2023-03-05');
 INSERT INTO tasks VALUES (default, 'an overdue task', 'overdue pending task', '2023-02-08', 'PENDING', '2023-02-01');
-INSERT INTO tasks VALUES (default, 'a cancelled task', 'example cancelled task', '2023-03-02', 'CANCELED', '2023-03-01');
-INSERT INTO tasks VALUES (default, 'a completed task!', 'example completed task', '2023-03-02', 'COMPLETED', '2023-03-01');
+--INSERT INTO tasks VALUES (default, 'a cancelled task', 'example cancelled task', '2023-03-02', 'CANCELED', '2023-03-01');
+--INSERT INTO tasks VALUES (default, 'a completed task!', 'example completed task', '2023-03-02', 'COMPLETED', '2023-03-01');
